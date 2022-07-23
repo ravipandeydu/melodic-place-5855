528d2829538f69c6fa6677217fe862b0a8fbff8e
ravipandeydu <rp120896@gmail.com>
2022-07-24 01:06:53 +0530

    Day 4 Added

---
+++ loseit/src/Components/Common/carouselData.js
@@ -0,0 +1,26 @@
+export const SlideData = [
+    {
+        image1: "https://assets.loseit.com/website/home/Features_WaterTracking.svg",
+        image2: "https://assets.loseit.com/website/home/Features_Macros.svg",
+        heading1: "Water Tracking",
+        heading2: "Macro Tracking",
+        text1: "Track your water intake to make sure you're getting the recommended amount daily.",
+        text2: "Set personalized macronutrient goals to ensure you’re getting enough protein, carbs, and more."
+    },
+    {
+        image1: "https://assets.loseit.com/website/home/Features_WaterTracking.svg",
+        image2: "https://assets.loseit.com/website/home/Features_Macros.svg",
+        heading1: "Water Tracking",
+        heading2: "Macro Tracking",
+        text1: "Track your water intake to make sure you're getting the recommended amount daily.",
+        text2: "Set personalized macronutrient goals to ensure you’re getting enough protein, carbs, and more."
+    },
+    {
+        image1: "https://assets.loseit.com/website/home/Features_WaterTracking.svg",
+        image2: "https://assets.loseit.com/website/home/Features_Macros.svg",
+        heading1: "Water Tracking",
+        heading2: "Macro Tracking",
+        text1: "Track your water intake to make sure you're getting the recommended amount daily.",
+        text2: "Set personalized macronutrient goals to ensure you’re getting enough protein, carbs, and more."
+    }
+]
